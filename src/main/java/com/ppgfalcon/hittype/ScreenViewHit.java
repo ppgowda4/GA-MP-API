e653b980e46555e6d179bd5dda597b2be81330fc
punithgowdap <punithgowdap@gmail.com>
2016-01-13 22:33:08 +0530

    changed screen view hit and removed extra project folder(by mistake committed)

--- src/main/java/com/ppgfalcon/hittype/ScreenViewHit.java
+++ src/main/java/com/ppgfalcon/hittype/ScreenViewHit.java
@@ -1,16 +1,3 @@
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.ppgfalcon.hittype;
 
 import com.ppgfalcon.transport.GoogleAnalyticsRequest;
@@ -23,19 +10,19 @@ import com.ppgfalcon.transport.GoogleAnalyticsRequest;
  * @author Punith
  */
 public class ScreenViewHit extends GoogleAnalyticsRequest<ScreenViewHit> {
-	public ScreenViewHit() {
-		this(null, null, null);
-	}
+    public ScreenViewHit() {
+        this(null, null, null);
+    }
 
-	public ScreenViewHit(String url, String title) {
-		this(url, title, null);
-	}
+    public ScreenViewHit(String url, String title) {
+        this(url, title, null);
+    }
 
-	public ScreenViewHit(String screenTiltle, String screenPath, String description) {
-		super("screenview");
-		contentDescription(screenTiltle+ "-" +screenPath);
-		documentHostName(screenTiltle);
-		documentPath(screenPath);
-		//contentDescription(description);
-	}
+    public ScreenViewHit(String screenTiltle, String screenPath, String description) {
+        super("screenview");
+        contentDescription(screenTiltle + screenPath);
+        documentHostName(screenTiltle);
+        documentPath(screenPath);
+        //contentDescription(description);
+    }
 }
